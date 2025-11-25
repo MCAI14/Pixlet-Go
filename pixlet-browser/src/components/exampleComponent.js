c1e15b1b7bb6edde7d38f885f83a207c9d630437
MCAI14 <misha14@outlook.pt>
2025-11-25 13:23:25 +0000

    Let´s make in Python!

--- pixlet-browser/src/components/exampleComponent.js
+++
@@ -1,8 +0,0 @@
-export const ExampleComponent = () => {
-    return (
-        <div>
-            <h1>Hello from Example Component!</h1>
-            <p>This is a simple example component.</p>
-        </div>
-    );
-};
